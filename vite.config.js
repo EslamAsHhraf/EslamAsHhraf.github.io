a04dcd5494be67120d7a3518e341071741b396ab
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2024-01-29 17:52:04 +0200

    edit configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext",
+  },
+  server: {
+    mimeType: {
+      ".js": "application/javascript",
+      ".mjs": "application/javascript",
+      ".jsx": "application/javascript",
+      ".tsx": "application/javascript",
+    },
+  },
+});
